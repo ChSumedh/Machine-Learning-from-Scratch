d2694762452ffba6b998063801ab8dc4e1b0967d
Sumedh <cherukurinagasumedh@gmail.com>
2026-01-28 10:26:40 +0530

    changes in rmse and added accuracy score

--- MLS/Metrics.py
+++ MLS/Metrics.py
@@ -39,9 +39,24 @@ def classification_report(y,y_t):
         cr[i][1]=cm.loc[classes[i],classes[i]]/np.sum(cm.loc[classes[i],:])
     cr=pd.DataFrame(cr,columns=["Precision","Recall"],index=classes)
     return cr
+
+def accuracy_score(y,y_t):
+    unique_y = np.unique(y)
+    unique_y_t = np.unique(y_t)
+    classes = np.unique(np.concatenate([unique_y, unique_y_t]))
+    classes=list(classes)
+    cm=confusion_matrix(y,y_t)
+    sum=np.sum(np.sum(cm))
+    num=0
+    for i in range(len(classes)):
+        num+=cm.loc[classes[i],classes[i]]
+    if(sum==0):
+        return 0
+    return num/sum
+
     
 def rmse(y,y_t):
     Y_checker(y,y_t)
-    if not (np.issubdtype(y,np.number) and np.issubdtype(y_t,np.number)):
+    if not (np.issubdtype(y.dtype,np.number) and np.issubdtype(y_t.dtype,np.number)):
         raise ValueError("The inputs have to be numbers")
     return np.sqrt(np.mean(np.square(y-y_t)))
