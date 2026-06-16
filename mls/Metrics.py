numerical bug fixes

mls/Metrics.py
@@ -36,8 +36,19 @@ def classification_report(y,y_t):
     cr=np.zeros((len(classes),3))
     for i in range(len(classes)):
         cr[i][0]=cm.loc[classes[i],classes[i]]/np.sum(cm[cm.columns[i]])
-        cr[i][1]=cm.loc[classes[i],classes[i]]/np.sum(cm.loc[classes[i],:])
-        cr[i][2]=2/((1/cr[i][0])+(1/cr[i][1]))
+        row_sum = np.sum(cm.loc[classes[i], :].values)
+
+        if row_sum == 0:
+            cr[i][1] = 0.0
+        else:
+            cr[i][1] = cm.loc[classes[i], classes[i]] / row_sum
+        p = cr[i][0]
+        r = cr[i][1]
+
+        if p + r == 0:
+            cr[i][2] = 0.0
+        else:
+            cr[i][2] = 2 * p * r / (p + r)
     cr=pd.DataFrame(cr,columns=["Precision","Recall","F1-Score"],index=classes)
     return cr
 

mls/Tree.py
@@ -139,11 +139,18 @@ class DecisionTreeClassifier:
         
         child_y1=np.bincount(childNode.y1,minlength=self.class_length)
         child_y2=np.bincount(childNode.y2,minlength=self.class_length)
+        
+        y1_sum = np.sum(child_y1)
+        y2_sum = np.sum(child_y2)
+        total = y1_sum + y2_sum
 
-        postSplitImp=np.sum(child_y1)*(1-np.sum(np.square(child_y1/np.sum(child_y1))))
-        postSplitImp=postSplitImp+np.sum(child_y2)*(1-np.sum(np.square(child_y2/np.sum(child_y2))))
-        postSplitImp=postSplitImp/(np.sum(child_y1) + np.sum(child_y2))
+        if total == 0:
+            postSplitImp = 0.0
+        else:
+            imp1 = 1 - np.sum(np.square(child_y1 / y1_sum)) if y1_sum != 0 else 0.0
+            imp2 = 1 - np.sum(np.square(child_y2 / y2_sum)) if y2_sum != 0 else 0.0
 
+            postSplitImp = (y1_sum * imp1 + y2_sum * imp2) / total
         return postSplitImp<preSplitImp
     
     def _buildTree(self,X,y,parentNode: _TreeNode,depth,maxDepth,minSamples):